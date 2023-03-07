5479b2c15f5258e44be0decba07075b5b597669f
mahesh95prabhu <101814052+mahesh95prabhu@users.noreply.github.com>
2023-03-07 10:49:47 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,16 +6,19 @@ import (
 	"net/http"
 )
 
+//Create the struct for user
 type User struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
 }
+
 
 func main() {
 	http.HandleFunc("/users", handleUsers)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
+// this funtion routes the request to the correct function depending on request type
 func handleUsers(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
@@ -26,7 +29,7 @@ func handleUsers(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
-
+//This is the function for GET requests, which will serve the users details mentioned below
 func getUsers(w http.ResponseWriter, r *http.Request) {
 	users := []User{
 		{Name: "Alice", Age: 23},
@@ -35,7 +38,7 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(users)
 }
-
+//This is the function for POST requests, which will create a user as per the provided details
 func createUser(w http.ResponseWriter, r *http.Request) {
 	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
